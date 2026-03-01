bbb8d55f02108f4406a491af99510014b54ee128
PAVAN2627 <pavanmali0281@gmail.com>
2026-03-01 11:43:52 +0530

    Update background

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
           playsInline
           className="w-full h-full object-cover"
         >
-          <source src="/cinematic-hackathon-scene-generation_3sPFUDst.mp4" type="video/mp4" />
+          <source src="/videobackground.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/50" />
       </div>
